fb78cc34162991886df6080fad529aad3eaff9d1
danielaruizdiaz <daniela.ruizdiaz@davinci.edu.ar>
2023-10-06 16:20:13 -0300

    portfolio

---
+++ script.js
@@ -0,0 +1,28 @@
+var animation = bodymovin.loadAnimation({
+	
+	container: document.getElementById('anim'),
+	rederer: 'svg',
+	loop: true,
+	autoplay: true,
+	path: 'data.json'
+})
+
+
+var animation = bodymovin.loadAnimation({
+	
+	container: document.getElementById('work'),
+	rederer: 'svg',
+	loop: true,
+	autoplay: true,
+	path: 'work.json'
+})
+
+
+var animation = bodymovin.loadAnimation({
+	
+	container: document.getElementById('cv'),
+	rederer: 'svg',
+	loop: true,
+	autoplay: true,
+	path: 'cv.json'
+})
